9cf220e6290298657fa1e3c832e37f08c1ecc737
Lân Bùi Tấn <tanlanhcmus@gmail.com>
2022-04-16 15:24:50 +0700

    chore(client): update to react 18

--- client/src/app/features/Catalog.tsx
+++ client/src/app/features/Catalog.tsx
@@ -1,14 +1,16 @@
 import { useState, useEffect } from "react";
 import {Product} from "../models/product";
 import ProductList from "./ProductList";
+import axios from "axios";
 
 export default function Catalog() {
     const [products, setProducts] = useState<Product[]>([]);
 
     useEffect(() => {
-        fetch('http://localhost:5000/api/products')
-            .then(response => response.json())
-            .then(data => setProducts(data))
+        (async function getProduct(){
+            const products = await axios.get<Product[]>('https://localhost:5000/api/products');
+            setProducts(products.data)
+        })()
     }, [])
 
     return (
