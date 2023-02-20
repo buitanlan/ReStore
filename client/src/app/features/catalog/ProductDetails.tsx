21a3bdd8ee8b2430208c82d6f053baea1bd02385
Bùi Tấn Lân <tanlanhcmus@gmail.com>
2023-02-21 00:50:16 +0700

    feat(): fetch a product

--- client/src/app/features/catalog/ProductDetails.tsx
+++ client/src/app/features/catalog/ProductDetails.tsx
@@ -1,5 +1,24 @@
 import { Typography } from '@mui/material';
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Product } from '../../models/product';
+import axios from 'axios';
 
 export default function ProductDetails() {
-  return <Typography variant="h2">Product details</Typography>;
+  const { id } = useParams<{ id: string }>();
+  const [product, setProduct] = useState<Product | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get<Product>(`https://localhost:5000/api/products/${id}`)
+      .then((res) => setProduct(res.data))
+      .catch(console.error)
+      .finally(() => setLoading(false));
+  }, [id]);
+
+  if (loading) return <h3>Loading...</h3>;
+
+  if (!product) return <h3>Product not found</h3>;
+  return <Typography variant="h2">{product.name}</Typography>;
 }
