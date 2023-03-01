584499b1d2fd54ad3c66ff835fcf96b3d3cb33d5
Lân Bùi Tấn <tanlanhcmus@gmail.com>
2023-03-01 15:08:58 +0700

    feat(): add paging sorting filter be

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,5 +1,7 @@
 ﻿using API.Data;
 using API.Entities;
+using API.Extensions;
+using API.RequestHelper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,23 +12,45 @@ namespace API.Controllers;
 public class ProductsController : BaseApiController
 {
     private readonly StoreContext _context;
+
     public ProductsController(StoreContext context)
     {
         _context = context;
     }
 
     [HttpGet]
-    public async Task<ActionResult<List<Product>>> GetProducts()
+    public async Task<ActionResult<PagedList<Product>>> GetProducts([FromQuery] ProductParams productParams)
     {
-        return await _context.Products.AsNoTracking().ToListAsync();
+        var query = _context.Products
+            .Sort(productParams.OrderBy)
+            .Search(productParams.SearchTerm)
+            .Filter(productParams.Brands, productParams.Types)
+            .AsQueryable();
+
+        var products = await PagedList<Product>.ToPagedList(query, productParams.PageNumber,
+            productParams.PageSize);
+
+        Response.AddPaginationHeader(products.MetaData);
+
+        return products;
     }
 
-    [HttpGet("{id:int}")]
+    [HttpGet("{id}")]
     public async Task<ActionResult<Product>> GetProduct(int id)
     {
-        var product = await _context.Products.AsNoTracking().SingleOrDefaultAsync(x => x.Id == id);
-        if (product == null) return NotFound();
-        return product;
+        var product = await _context.Products.FindAsync(id);
 
+        if (product == null) return NotFound();
+
+        return product;
+    }
+
+    [HttpGet("filters")]
+    public async Task<IActionResult> GetFilters()
+    {
+        var brands = await _context.Products.Select(p => p.Brand).Distinct().AsNoTracking().ToListAsync();
+        var types = await _context.Products.Select(p => p.Type).Distinct().AsNoTracking().ToListAsync();
+
+        return Ok(new { brands, types });
     }
 }
